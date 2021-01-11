379c9dc8b7ecc0b5e5a2fc4dff44ad9e7b901876
jc <jcmpes@gmail.com>
2021-01-11 21:16:22 +0100

    Create models for invoices and providers

--- facturas/app/models.py
+++ facturas/app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext as _
+import datetime
 
 # Create your models here.
+class Proveedor(models.Model):
+    # Fields
+    codigo = models.IntegerField()
+    nombre = models.CharField(max_length=16)
+    comentarios = models.TextField()
+
+    # Metadata
+    class Meta:
+        ordering = ['codigo']
+
+    # Methods
+    def __str__(self):
+        return self.codigo
+
+
+class Factura(models.Model):
+    # Fields
+    codigo = models.CharField(max_length=32, help_text='Introduce el código de la factura')
+    autor = models.CharField(max_length=16)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.PROTECT)
+    fecha_creacion = models.DateField(_("Date"), default=datetime.date.today)
+    fecha_vencimiento = models.DateField(null=True, blank=True)
+
+    # Metadata
+    class Meta:
+        ordering = ['-fecha_creacion']
+
+    # Methods
+    def __str__(self):
+        return self.codigo
+
--- facturas/facturas/urls.py
+++ facturas/facturas/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
-    path('', RedirectView.as_view(url='app/', permanent=True))
+    path('', RedirectView.as_view(url='app/', permanent=False))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
