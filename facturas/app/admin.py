7229f143593aee2e8f5822bbfb3b98a2acbb2e71
jc <jcmpes@gmail.com>
2021-01-12 22:47:22 +0100

    Register a ModelAdmin class for Factura model

--- facturas/app/admin.py
+++ facturas/app/admin.py
@@ -3,4 +3,11 @@ from .models import Proveedor, Factura
 
 # Register your models here.
 admin.site.register(Proveedor)
-admin.site.register(Factura)
+# admin.site.register(Factura)
+
+# Dedine the admin class for Factura
+class FacturaAdmin(admin.ModelAdmin):
+    pass
+
+# Register the admin class for Factura with the model Factura
+admin.site.register(Factura, FacturaAdmin)
