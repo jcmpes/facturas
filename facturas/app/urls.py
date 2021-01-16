318d57fe5ac61e60dd19fb3c81f95baaa1eaf277
jc <jcmpes@gmail.com>
2021-01-16 09:31:36 +0100

    Added index function in views.py

--- facturas/app/urls.py
+++ facturas/app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# Create your views here
 urlpatterns = [
     path('clear_cache/', views.clear_cache),
+    path('', views.index, name='index'),
 ]
+
--- facturas/app/views.py
+++ facturas/app/views.py
@@ -3,6 +3,7 @@ from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.views.decorators.cache import never_cache
+from app.models import Factura, Proveedor
 
 # Create your views here.
 @never_cache
@@ -11,3 +12,16 @@ def clear_cache(request):
         raise PermissionDenied
     cache.clear()
     return HttpResponse('Cache has been cleared')
+
+def index(request):
+    """ View function for home page of the site """
+
+    num_invoices = Factura.objects.all().count()
+    num_providers = Proveedor.objects.all().count()
+
+    context = {
+        'num_invoices': num_invoices,
+        'num_providers': num_providers,
+    }
+
+    return render(request, 'index.html', context=context)
